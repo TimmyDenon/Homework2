336f2edf0dfddeb67626be154864e984df6823ff
TimmyDenon <114209281+TimmyDenon@users.noreply.github.com>
2022-10-07 15:30:31 +0800

    Update Main.c

--- HM2 3.17/source/Main.c
+++ HM2 3.17/source/Main.c
@@ -7,7 +7,10 @@ int main(void)
 {	
 	while (x!=-1)
 	{
+                while(true)
+              {
 		printf("Enter account unmber (-1 to end): "); scanf_s("%d", &x);
+                while(x==(-1))break;
 		printf("Enter beginning balance: "); scanf_s("%f", &a);
 		printf("Enter total charges: "); scanf_s("%f", &b);
 		printf("Enter total credits: "); scanf_s("%f", &c);
@@ -17,6 +20,7 @@ int main(void)
 		printf("Blance: %0.2f\n",(a+b-c));
 		if (((a + b - c) > d))
 		printf("Credit Limit Exceeded\n");
+               }
 		system("pause");
 	}
 	return 0;
