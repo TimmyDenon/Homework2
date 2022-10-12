fbb941a0c172a3ba6a621b99de27943f1c2c8055
TimmyDenon <popomei9999@gmail.com>
2022-10-12 09:45:22 +0800

    3.17 comple

--- HM2 3.17/source/Main.c
+++ HM2 3.17/source/Main.c
@@ -5,23 +5,23 @@ float a, b, c, d;
 int x;
 int main(void)
 {	
-	while (x!=-1)
-	{
-                while(true)
-              {
+
+	
+	while(1)
+    {
 		printf("Enter account unmber (-1 to end): "); scanf_s("%d", &x);
-                while(x==(-1))break;
+		if (x == (-1))break;
 		printf("Enter beginning balance: "); scanf_s("%f", &a);
 		printf("Enter total charges: "); scanf_s("%f", &b);
 		printf("Enter total credits: "); scanf_s("%f", &c);
 		printf("Enter credit limit: "); scanf_s("%f", &d);
 		printf("Account: %d\n", x);
 		printf("Credit limit: %0.2f\n", d);
-		printf("Blance: %0.2f\n",(a+b-c));
+		printf("Blance: %0.2f\n", (a + b - c));
 		if (((a + b - c) > d))
-		printf("Credit Limit Exceeded\n");
-               }
-		system("pause");
-	}
+			printf("Credit Limit Exceeded\n");
+    }
+	system("pause");
+	
 	return 0;
 }
